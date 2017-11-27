3035996b9b1991d82ce28ab32734ee6f5275b6da
chamow97 <jchandramowli15039@it.ssn.edu.in>
2017-11-27 12:18:14 +0530

    Add first day :sparkles:

---
+++ Day1/index.js
@@ -0,0 +1,30 @@
+
+
+function playSound(e){
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  if(!audio)
+  {
+    // if key pressed is not correct
+    return;
+  }
+  //rewind to start each time pressed for prolonged sound
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
+
+}
+
+function removeTransition(e){
+  if(e.propertyName !== 'transform')
+  {
+    return;
+  }
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
